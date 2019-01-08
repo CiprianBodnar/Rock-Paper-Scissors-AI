71dcd8591e86686835d0b9b5ab52ec415f0d972f
Ciprian <cyprian.bodnar@gmail.com>
2019-01-08 19:51:22 +0200

    pattern representation changed

--- ai_turn.py
+++ ai_turn.py
@@ -2,69 +2,80 @@
 # 1 - hartie
 # 2 - piatra
 import random
+
+def get_random():
+    r = random.randint(0,2)
+    if r ==0:
+        return 'F'
+    elif r == 1:
+        return 'H'
+    else:
+        return 'P'
+
 def easy_mode():
-    return random.randint(0,2)
+    get_random()
+
 
 
 def medium_mode(prob_dict,pattern_dict):
     # 50% random , 50% anticipare
-    if pattern_dict == False or prob_dict == False:
-        return random.randint(0,2)
+    sum_of_patterns = 0
+    for elem in pattern_dict:
+        sum_of_patterns = sum_of_patterns + pattern_dict[elem]
+
+    if sum_of_patterns<3:
+        get_random()
+
+
+    random_move = random.randint(0,2)
+    if random_move == 0:
+        random_move = 'F'
+    elif random_move == 1:
+        random_move = 'H'
     else:
-        sum_of_patterns = 0
-        for elem in pattern_dict:
-            sum_of_patterns = sum_of_patterns + pattern_dict[elem]
-
-        if sum_of_patterns<3:
-            return random.randint(0,2)
-
-
-        random_move = random.randint(0,2)
-        
-        max_prob = 0
-        pattern = None
-        
-        for elem in pattern_dict:
-            if pattern_dict[elem] > max_prob and int ( elem[0] ) == random_move:
-                max_prob = pattern_dict[elem]
-                pattern = elem
-        if pattern == None:
-            return random.randint(0,2)
-        return pattern[1]
+        random_move = 'P'
+    
+    max_prob = 0
+    pattern = None
+    
+    for elem in pattern_dict:
+        if pattern_dict[elem] > max_prob and elem[0] == random_move:
+            max_prob = pattern_dict[elem]
+            pattern = elem
+    if pattern == None:
+        get_random()
+    return pattern[1]
                 
                 
 def hard_mode(prob_dict,pattern_dict):
     
-    if pattern_dict == False or prob_dict == False:
-        return random.randint(0,2)
-    else:
-        sum_of_patterns = 0
-        for elem in pattern_dict:
-            sum_of_patterns = sum_of_patterns + pattern_dict[elem]
+    sum_of_patterns = 0
+    for elem in pattern_dict:
+        sum_of_patterns = sum_of_patterns + pattern_dict[elem]
 
-        if sum_of_patterns<3:
-            return random.randint(0,2)
+    if sum_of_patterns<3:
+        get_random()
+    
+    maxim = 0
+    pattern = None
+
+    max_prob = 0
+    tip = 0
+    for elem in prob_dict:
         
-        maxim = 0
-        pattern = None
+        if prob_dict[elem]> max_prob:
+            max_prob = prob_dict[elem]
+            tip = elem
 
-        max_prob = 0
-        tip = 0
-        for elem in prob_dict:
+
+    for elem in pattern_dict:
+        if pattern_dict[elem] > maxim and  (elem[0]) == (tip):
             
-            if prob_dict[elem]> max_prob:
-                max_prob = prob_dict[elem]
-                tip = (elem)
-
-
-        for elem in pattern_dict:
-            if pattern_dict[elem] > maxim and int (elem[0]) == int(tip):
-               
-                maxim = pattern_dict[elem]
-                pattern = elem
-        if pattern == None:
-            return random.randint(0,2)
-        return pattern[1]
+            maxim = pattern_dict[elem]
+            pattern = elem
+    if pattern == None:
+        get_random()
+    return pattern[1]
 
 def strategy(prob_dict,pattern_dict,game_mode):
 
